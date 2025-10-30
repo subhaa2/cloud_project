e473151d18a1727c79d96623020b87a8da38e30d
Shyann Lai <2402456@sit.singaporetech.edu.sg>
2025-10-31 02:01:49 +0800

    Added the whiteboard codes into its respective docker folders and made changes
    Changes:
    - (whiteboard) standardised internal port for to 8080 and external port to 4001
    -- Opens port 4001 on host machine (your laptop) and directs traffic to port 8080 inside the container.
    - Both frontend's and flashcard's Dockerfile temporarily have placeholder commands
    - K8's whiteboard deployment/service.yaml files standardise port to 8080 for containerisation
    
    to test:
    - in realtime\whiteboard directory --> npm install
    - in root directory --> docker compose up --build -d
    - open 'http://localhost:4001' in browser to check the server is running correctly inside the Docker container and the port mapping is working
    - Open static HTML (whiteboard.html) file from File Explorer
    -- console should show "✅ Connected to server"
    - (track logs)> docker logs realtime-whiteboard

---
+++ realtime/whiteboard/src/whiteboard_server.js
@@ -0,0 +1,55 @@
+
+// server.js
+import express from "express";
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
+
+const paths = []; // store all drawing paths
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+const PORT = process.env.PORT || 8080;
+
+// Keep a list of connected clients
+wss.on("connection", (socket) => {
+    console.log(" New client connected");
+
+    // Send all existing paths to the new clients
+    paths.forEach((path) => socket.send(JSON.stringify({ type: "draw", path})));
+
+    socket.on("message", (message) => {
+        try {
+            const data = JSON.parse(message);
+
+            // Save new strokes
+            if (data.type == "draw") {
+                paths.push(data.path);
+            }
+
+            // Clear all existing strokes
+            if (data.type == "clear"){
+                paths.length = 0;
+            }
+
+            // Broadcast to all others
+            wss.clients.forEach((client) => {
+                if (client !== socket && client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify(data));
+                }
+            });
+        } catch (err) {
+            console.error("Error parsing message:", err);
+        }
+    });
+
+    socket.on("close", () => {
+        console.log(" Client disconnected");
+    });
+});
+
+//  Simple health check
+app.get("/", (_, res) => res.send("WebSocket whiteboard server is running."));
+
+server.listen(PORT,() => console.log(`Server running on port ${PORT}`));
